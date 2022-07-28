6cf99ed30e23f4d12a18823e3845b20d8dc276f4
tasinnahian <tasin.nahian@gmail.com>
2022-07-28 12:30:00 +0600

    inserstion code added

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -1,3 +1,49 @@
 public class LinkedList {
+    Node head;
+
+    static class Node //make the class to static otherwise static class cannot call non static class
+    {
+        int data;
+        Node next;
+        
+        Node(int d)
+        {
+            this.data = d;
+            next = null;
+        }
+    }
+
+    public void printList()
+    {
+        Node n = head;
+        while(n != null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
     
+
+    public static void main(String args[])
+    {
+        LinkedList list = new LinkedList();
+
+        list.head = new Node(5);
+        Node second = new Node(7);
+        Node third = new Node(10);
+
+        //Connecting the next node to the other nodes
+        list.head.next = second;
+        second.next = third;
+
+        list.printList();
+    }
+
+
 }
+/* 
+Time Complexity	 Worst Case	   Average Case
+Search	           O(n)	          O(n)
+Insert	           O(1)	          O(1)
+Deletion	       O(1)           O(1) 
+*/
---
+++ Linked_List/LinkedListInsertion.java
@@ -0,0 +1,66 @@
+// Instertion at first node in LinkedList
+public class LinkedListInsertion
+{
+	Node head;
+	int count = 0;
+
+	// creation of nodes
+	class Node
+	{ 
+		int data;
+		Node next;
+
+		Node(int d)
+		{
+			this.data = d;
+			next = null;
+		}
+
+	}
+
+	//push into linked list
+	public void push(int new_data)
+	{
+		if(head == null)
+		{
+			head = new Node(new_data);
+		}
+		else
+		{
+		Node new_node = new Node(new_data);
+		new_node.next = head;
+		head = new_node;
+		}
+	}
+	public void printList()
+	{
+		Node n = head;
+		
+		while(n != null)
+		{
+			System.out.print(n.data + " ");
+			n = n.next;
+			count++;
+		}
+	}
+	public static void main(String args [])
+	{
+		LinkedListInsertion list = new LinkedListInsertion();
+		//as it is LinkedList insertion therefore every value 
+		//will be inserted fist and first value inserted will be 
+		//at the last
+		list.push(1);
+		list.push(2);
+		list.push(3);
+		list.push(4);
+		list.push(5);
+		list.push(6);
+		list.push(7);
+
+		list. printList();
+		System.out.println();
+		System.out.println(list.count);
+
+
+	}
+}
